056e7ad6287385afb69a438d53379b7838203aef
Gene De Lisa <gene@rockhoppertech.com>
2017-10-26 12:39:08 -0400

    initial

---
+++ README.md
@@ -0,0 +1,55 @@
+# NSTextField validation
+
+https://github.com/genedelisa/TextFieldValidation.git
+
+[![license](https://img.shields.io/github/license/mashape/apistatus.svg)](https://en.wikipedia.org/wiki/MIT_License)
+[![GitHub version](https://badge.fury.io/gh/genedelisa%2Fnstextfield-validation.svg)](http://badge.fury.io/gh/genedelisa%2Fnstextfield-validation)
+[![GitHub tag](https://img.shields.io/github/tag/genedelisa/nstextfield-validation.svg)](https://github.com/genedelisa/nstextfield-validation/)
+[![GitHub release](https://img.shields.io/github/release/genedelisa/nstextfield-validation.svg)](https://github.com/genedelisa/nstextfield-validation/)
+
+
+[![Swift 4](https://img.shields.io/badge/swift4-compatible-4BC51D.svg?style=flat")](https://developer.apple.com/swift)
+
+
+[![Platforms OSX](https://img.shields.io/badge/Platforms-OSX-lightgray.svg?style=flat)](https://swift.org/)
+
+[![Sponsors](https://img.shields.io/badge/Sponsors-Rockhopper%20Technologies-orange.svg?style=flat)](http://www.rockhoppertech.com/)
+[![Twitter @GeneDeLisaDev](https://img.shields.io/twitter/follow/GeneDeLisaDev.svg?style=social)](https://twitter.com/GeneDeLisaDev)
+
+
+An example using validation on NSTextFields with Cocoa bindings in Swift.
+
+
+## Blog post for this example.
+
+[Blog post](http://www.rockhoppertech.com/blog/nstextfield-validation/)
+
+
+
+## Bugs
+
+Don't see any. Do you?
+
+[![GitHub issues](https://img.shields.io/github/issues/genedelisa/nstextfield-validation.svg)](https://github.com/genedelisa/nstextfield-validation/issues)
+
+If you find one, [please add it to issues](https://github.com/genedelisa/nstextfield-validation/issues)
+
+
+
+### Buy my kitty Giacomo some cat food
+
+[![paypal](https://www.paypalobjects.com/en_US/i/btn/btn_donate_SM.gif)](https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=F5KE9Z29MH8YQ&bnP-DonationsBF:btn_donate_SM.gif:NonHosted)
+
+<img src="http://www.rockhoppertech.com/blog/wp-content/uploads/2016/07/momocoding-1024.png" alt="Giacomo Kitty" width="400" height="300">
+
+## Licensing
+
+[MIT](https://en.wikipedia.org/wiki/MIT_License)
+
+Please read the [LICENSE](LICENSE) for details.
+
+## Credits
+
+*	[Gene De Lisa's development blog](http://rockhoppertech.com/blog/)
+*	[Gene De Lisa's music blog](http://genedelisa.com/)
+*   Twitter: [@GeneDeLisaDev](http://twitter.com/genedelisadev)
--- TextFieldValidation/ViewController.swift
+++ TextFieldValidation/ViewController.swift
@@ -8,12 +8,27 @@
 
 import Cocoa
 
-class ViewController: NSViewController {
+class FRNSTextField : NSTextField {
+    override var acceptsFirstResponder: Bool { return true }
+}
 
+@objcMembers
+class ViewController: NSViewController {
+    
+    @IBOutlet var givenNameTextField: NSTextField!
+    
+    @IBOutlet var familyNameTextField: NSTextField!
+    
+    @IBOutlet var ageTextField: NSTextField!
+    
+    @IBOutlet var submitButton: NSButton!
+    
+    @objc dynamic var givenName:String?
+    @objc dynamic var familyName:String?
+    @objc dynamic var age:Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override var representedObject: Any? {
@@ -22,6 +37,138 @@ class ViewController: NSViewController {
         }
     }
 
+    @IBAction func submitAction(_ sender: NSButton) {
+        self.givenName = givenNameTextField.stringValue
+        self.familyName = familyNameTextField.stringValue
+        if let age = Int(ageTextField.stringValue) {
+            self.age = age
+        } else {
+            displayAlert(messageText: "Invalid Age value",
+                         informativeText: "'\(ageTextField.stringValue)' is a bad number")
+        }
+        Swift.print("""
+            given:\(self.givenName ?? "No given")
+            family: \(self.familyName ?? "No family")
+            age: \(self.age)
+            """)
+    }
+    
+    func displayAlert(messageText: String, informativeText: String) {
+        let alert = NSAlert()
+        alert.messageText = messageText
+        alert.informativeText = informativeText
+        alert.alertStyle = .warning
+        alert.addButton(withTitle: NSLocalizedString("OK", comment: "OK"))
+        alert.runModal()
+    }
+    
+    func displayAlert(error:Error) {
+        let alert = NSAlert(error: error)
+        alert.alertStyle = .critical
+        alert.showsHelp = true
+        alert.runModal()
+    }
+    
+    
+    override func validateValue(_ ioValue: AutoreleasingUnsafeMutablePointer<AnyObject?>, forKey inKey: String) throws {
+        Swift.print("\(#function)")
+        Swift.print("inKey \(inKey)")
+
+        switch inKey {
+        case "givenName" :
+            Swift.print("validating givenName")
+            if let s = ioValue.pointee as? String {
+                Swift.print("s \(s)")
+                if s.count < 3 {
+                    throw DBError.textFieldValidation("Oh bloody hell!\nA given name must be at least three characters long")
+                }
+            }
+        case "familyName" :
+            Swift.print("validating familyName")
+            if let s = ioValue.pointee as? String {
+                Swift.print("s \(s)")
+                if s.count < 3 {
+//                    throw DBError.textFieldValidation("Oh bloody hell!\nA family name must be at least three characters long")
+                    let error = DBError.textFieldValidation("Oh bloody hell!\nA family name must be at least three characters long")
+                    displayAlert(error: error)
+                }
+            }
+        case "age" :
+            Swift.print("validating age")
+            if ioValue.pointee == nil {
+                throw DBError.textFieldValidation("Oh bloody hell!\n\nEnter a number!")
+
+            }
+            if let s = ioValue.pointee as? String {
+                let onlyDigits = CharacterSet.decimalDigits.inverted
+                if s.rangeOfCharacter(from: onlyDigits) != nil {
+                    throw DBError.textFieldValidation("Oh bloody hell!\n'\(s)' is not a number.\nEnter a number!")
+                } else {
+                    Swift.print("dig \(s)")
+                }
+            }
+            
+        default: break
+        }
+    }
 
 }
+
+//MARK: - DBError
+enum DBError : Error {
+    case textFieldValidation(String)
+}
+
+extension DBError: LocalizedError {
+    public var errorDescription: String? {
+        switch self {
+        case let .textFieldValidation(message):
+            return NSLocalizedString(message,
+                                     comment: "My error")
+        }
+    }
+    
+    public var failureReason: String? {
+        switch self {
+        case .textFieldValidation:
+            return NSLocalizedString("I don't know why.", comment: "")
+        }
+    }
+    
+    public var recoverySuggestion: String? {
+        switch self {
+        case .textFieldValidation:
+            return NSLocalizedString("Plug it in!.", comment: "")
+        }
+    }
+    
+    public var helpAnchor: String? {
+        switch self {
+        case .textFieldValidation:
+            return NSLocalizedString("someHelpAnchor.", comment: "")
+        }
+    }
+}
+
+extension DBError: CustomNSError {
+    
+    public static var errorDomain: String {
+        return "myDomain"
+    }
+    
+    public var errorCode: Int {
+        switch self {
+        case .textFieldValidation:
+            return 666
+        }
+    }
+    
+    public var errorUserInfo: [String : Any] {
+        switch self {
+        case .textFieldValidation:
+            return [ "line": 13]
+        }
+    }
+}
+
 
