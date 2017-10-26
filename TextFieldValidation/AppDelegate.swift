d21e67ce10726d17d66093bf8523c32b148bffb2
Gene De Lisa <gene@rockhoppertech.com>
2017-10-26 08:17:16 -0400

    Initial Commit

---
+++ TextFieldValidation/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TextFieldValidation
+//
+//  Created by Gene De Lisa on 10/26/17.
+//  Copyright © 2017 Gene De Lisa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TextFieldValidation/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TextFieldValidation
+//
+//  Created by Gene De Lisa on 10/26/17.
+//  Copyright © 2017 Gene De Lisa. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
